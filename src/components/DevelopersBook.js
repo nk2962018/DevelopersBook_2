ff3497fa700fd2058c7b695776f41cff36847458
Nisha <nkolivia1234@gmail.com>
2023-08-04 16:48:06 +0530

    Feat:display a book with title.

--- src/components/DevelopersBook.js
+++ src/components/DevelopersBook.js
@@ -1,9 +1,17 @@
 import { Constants } from '../constants/Constants';
 import '../styles/DevelopersBook.css';
+import cleanCode from '../Images/cleanCode.jpg'
 
 const  DevelopersBook = () => {
   return (
+    <>
       <div className='header' data-testid='header'>{Constants.HEADER}</div>
+      <div className='book' data-testid='book'>
+        <img src={cleanCode} alt='clean code'/>
+        <p>Clean Code</p>
+      </div>
+
+    </>
   );
 }
 
--- src/testcases/DevelopersBook.spec.js
+++ src/testcases/DevelopersBook.spec.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import DevelopersBook from '../components/DevelopersBook';
 import { TestConstants } from '../constants/TestConstants';
+import cleanCode from '../Images/cleanCode.jpg';
 
 describe('Developers Book works fine when', () => {
 
@@ -8,6 +9,24 @@ describe('Developers Book works fine when', () => {
     render(<DevelopersBook />);
     const header = screen.getByTestId('header');
     expect(header.textContent).toBe(TestConstants.HEADER);
+  });
+
+  it('displays img with correct alt text ' , () => {
+    render(<DevelopersBook />);
+    const bookAlt = screen.getByRole('img');
+    expect(bookAlt).toHaveAttribute('alt','clean code');
+  });
+
+  it('displays img with correct path ' , () => {
+    render(<DevelopersBook />);
+    const bookImg = screen.getByRole('img');
+    expect(bookImg).toHaveAttribute('src',cleanCode);
+  });
+
+  it('displays img with correct name of book ' , () => {
+    render(<DevelopersBook />);
+    const bookName = screen.getByText('Clean Code');
+    expect(bookName).toBeDefined();
   });
  
 });
