1a8e2170dd3f4dfc2662dd69f6321e5fd1187087
Nisha <nkolivia1234@gmail.com>
2023-08-05 13:36:08 +0530

    Refactor:restructure constants for better readability.

--- src/components/DevelopersBook.js
+++ src/components/DevelopersBook.js
@@ -1,20 +1,7 @@
-import { Constants } from '../constants/Constants';
+import { Constants, listOfBooks } from '../constants/Constants';
 import '../styles/DevelopersBook.css';
-import cleanCode from '../Images/cleanCode.jpg';
-import cleanCoder from '../Images/cleanCoder.jpg';
-import cleanArchitecture from '../Images/cleanArchitecture.jpg';
-import tdd from '../Images/TDD.jpg';
-import legacyCoder from '../Images/legacycode.jpg';
 
 const  DevelopersBook = () => {
-
-  const listOfBooks = [
-    {id:1, title:"Clean code", imgUrl:`${cleanCode}`}, 
-    {id:2, title:"The Clean coder", imgUrl:`${cleanCoder}`}, 
-    {id:3, title:"Clean Architecture", imgUrl:`${cleanArchitecture}`}, 
-    {id:4, title:"TDD by example", imgUrl:`${tdd}`}, 
-    {id:5, title:"Working effectively with legacy Code", imgUrl:`${legacyCoder}`}
-  ]
 
   const displayListOfAllBooks = (books) => {
     return books.map(book => 
