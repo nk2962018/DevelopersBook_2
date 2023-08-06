65a84e96353cde0a7ed9f7743d2ca816cbfa7104
Nisha <nkolivia1234@gmail.com>
2023-08-06 14:05:27 +0530

    Refactor:restructure constants for better readability.

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -7,13 +7,42 @@ import legacyCoder from '../Images/legacycode.jpg';
 export const Constants = {
     HEADER : 'Developers Book',
     COUNT_OF_BOOKS_WITH_UNIQUE_TITLES : 5,
-    ADD_TO_CART_BUTTON_TEXT : 'Add to Cart'
+    ADD_TO_CART_BUTTON_TEXT : 'Add to Cart',
+    BOOK_TITLE_CLEAN_CODE : 'Clean Code',
+    BOOK_TITLE_CLEAN_CODER : 'The Clean coder',
+    BOOK_TITLE_CLEAN_ARCHITECTURE : 'Clean Architecture',
+    BOOK_TITLE_TDD : 'TDD by example',
+    BOOK_TITLE_LEGACY_CODE : 'Working effectively with legacy Code'
 };
 
 export const listOfBooks = [
-    {id:1, title:"Clean code", imgUrl:`${cleanCode}`}, 
-    {id:2, title:"The Clean coder", imgUrl:`${cleanCoder}`}, 
-    {id:3, title:"Clean Architecture", imgUrl:`${cleanArchitecture}`}, 
-    {id:4, title:"TDD by example", imgUrl:`${tdd}`}, 
-    {id:5, title:"Working effectively with legacy Code", imgUrl:`${legacyCoder}`}
+    {
+        id:1, 
+        title:`${Constants.BOOK_TITLE_CLEAN_CODE}`, 
+        imgUrl:`${cleanCode}`, 
+    },
+
+    {
+        id:2, 
+        title:`${Constants.BOOK_TITLE_CLEAN_CODER}`, 
+        imgUrl:`${cleanCoder}`, 
+    }, 
+
+    {
+        id:3, 
+        title:`${Constants.BOOK_TITLE_CLEAN_ARCHITECTURE}`, 
+        imgUrl:`${cleanArchitecture}`, 
+    }, 
+
+    {
+        id:4, 
+        title:`${Constants.BOOK_TITLE_TDD}`, 
+        imgUrl:`${tdd}`, 
+    },
+
+    {
+        id:5, 
+        title:`${Constants.BOOK_TITLE_LEGACY_CODE}`, 
+        imgUrl:`${legacyCoder}`, 
+    }
 ]
--- src/constants/TestConstants.js
+++ src/constants/TestConstants.js
@@ -1,5 +1,48 @@
+import cleanCode from '../Images/cleanCode.jpg';
+import cleanCoder from '../Images/cleanCoder.jpg';
+import cleanArchitecture from '../Images/cleanArchitecture.jpg';
+import tdd from '../Images/TDD.jpg';
+import legacyCoder from '../Images/legacycode.jpg';
+
 export const TestConstants = {
     HEADER : 'Developers Book',
     COUNT_OF_BOOKS_WITH_UNIQUE_TITLES : 5,
-    ADD_TO_CART_BUTTON_TEXT : 'Add to Cart'
+    ADD_TO_CART_BUTTON_TEXT : 'Add to Cart',
+    BOOK_TITLE_CLEAN_CODE : 'Clean Code',
+    BOOK_TITLE_CLEAN_CODER : 'The Clean coder',
+    BOOK_TITLE_CLEAN_ARCHITECTURE : 'Clean Architecture',
+    BOOK_TITLE_TDD : 'TDD by example',
+    BOOK_TITLE_LEGACY_CODE : 'Working effectively with legacy Code'
 };
+
+export const listOfBooks = [
+    {
+        id:1, 
+        title:`${TestConstants.BOOK_TITLE_CLEAN_CODE}`, 
+        imgUrl:`${cleanCode}`, 
+    }, 
+
+    {   
+        id:2, 
+        title:`${TestConstants.BOOK_TITLE_CLEAN_CODE}`, 
+        imgUrl:`${cleanCoder}`, 
+    }, 
+
+    {
+        id:3, 
+        title:`${TestConstants.BOOK_TITLE_CLEAN_ARCHITECTURE}`, 
+        imgUrl:`${cleanArchitecture}`, 
+    }, 
+
+    {
+        id:4, 
+        title:`${TestConstants.BOOK_TITLE_TDD}`, 
+        imgUrl:`${tdd}`, 
+    }, 
+
+    {
+        id:5, 
+        title:`${TestConstants.BOOK_TITLE_LEGACY_CODE}`, 
+        imgUrl:`${legacyCoder}`, 
+    }
+]
